6310e6b521360cee1310800d7616a96e93272d03
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2024-01-21 17:52:17 +0700

    clean start

--- pam_fdkey.c
+++ pam_fdkey.c
@@ -5,41 +5,12 @@
 #include <string.h>
 #include <sys/syslog.h>
 
-#define TEST_FILE "/tmp/pam_fdkey_test"
-#define TEST_STRING "unlock:key"
+#define INIT_PROMPT "Insert USB device containing key and press enter."
+#define RETRY_PROMPT "Key not found. Retry? [(Y)es/(N)o/(P)assword]"
 
-PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    FILE *file;
-    char line[256];
-    const char *prompt = "Create authentication file and press enter.";
-    char *response = NULL;
-
-    // Display custom prompt
-    if (pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &response, "%s", prompt) != PAM_SUCCESS || response == NULL) {
-        // Error or user just pressed enter
-        return PAM_IGNORE;
-    }
-
-    // Free response after use
-    if (response != NULL) {
-        free(response);
-    }
-
-    // Open the file and check for the string
-    file = fopen(TEST_FILE, "r");
-    if (file != NULL) {
-        while (fgets(line, sizeof(line), file)) {
-            if (strstr(line, TEST_STRING) != NULL) {
-                fclose(file);
-                return PAM_SUCCESS;
-            }
-        }
-        fclose(file);
-    }
-
-    // The file is not found or does not contain the string
-    pam_info(pamh, "Authentication file not found.");
-    return PAM_IGNORE;
+extern int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    
+    return PAM_AUTH_ERR;
 }
 
 
@@ -47,6 +18,9 @@ extern int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc,
 						  const char **argv) {
 	return PAM_SUCCESS;
 }
+
+// get mounted flash drives
+
 
 #ifdef PAM_STATIC
 struct pam_module _pam_fdkey_modstruct = {
