ae88ae3c9f993f522c886ede8723b6a01c330200
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-01-25 11:51:05 +0700

    actual communication between module and daemon

--- pam_fdkey.c
+++ pam_fdkey.c
@@ -4,12 +4,89 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/syslog.h>
+#include <dbus/dbus.h>
+#include <pwd.h>
+#include <sys/types.h>
 
 #define INIT_PROMPT "Insert USB device containing key and press enter."
 #define RETRY_PROMPT "Key not found. Retry? [(Y)es/(N)o/(P)assword]"
 
+#define FALLBACK_MSG "Falling back to password authentication."
+
 extern int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    
+	// initialize D-Bus
+
+	DBusConnection* conn;
+	DBusError err;
+	DBusMessage *msg, *reply;
+	DBusMessageIter args;
+	DBusPendingCall* pending;
+
+	dbus_error_init(&err);
+
+	conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+	if (dbus_error_is_set(&err)) {
+		pam_error(pamh, "D-Bus connection error (%s)", err.message);
+		dbus_error_free(&err);
+
+		pam_info(pamh, FALLBACK_MSG);
+		return PAM_AUTH_ERR;
+	}
+
+	// get the username
+	const char* username;
+	int pam_user;
+
+	pam_user = pam_get_user(pamh, &username, NULL);
+	if (pam_user != PAM_SUCCESS) {
+		pam_error(pamh, "Failed to get username: (%s)", pam_user);
+		return PAM_USER_UNKNOWN;
+	}
+
+	// get the uid
+	unsigned int uid;
+
+	struct passwd* pwd = getpwnam(username);
+	if (pwd == NULL) {
+		pam_error(pamh, "Failed to get uid for username %s", username);
+		return PAM_USER_UNKNOWN;
+	}
+
+	uid = pwd->pw_uid;
+
+	pam_info(pamh, "uid: %d", uid);
+
+	// call D-Bus method PubKeyAuth(uid)
+	// in interface net.meowcorp.FDKey.Authenticate
+	// in object net.meowcorp.FDKey.Authentication
+	// in service net.meowcorp.FDKey
+
+	char* service = "net.meowcorp.FDKey";
+	char* object = "/net/meowcorp/FDKey/Authentication";
+	char* interface = "net.meowcorp.FDKey.Authenticate";
+	char* method = "PubKeyAuth";
+
+	msg = dbus_message_new_method_call(service, object, interface, method);
+	if (msg == NULL) {
+		pam_error(pamh, "Failed to create D-Bus message");
+		return PAM_AUTH_ERR;
+	}
+
+	dbus_message_iter_init_append(msg, &args);
+	if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_UINT32, &uid)) {
+		pam_error(pamh, "Failed to append uid to D-Bus message");
+		return PAM_AUTH_ERR;
+	}
+
+	reply = dbus_connection_send_with_reply_and_block(conn, msg, -1, &err);
+	dbus_message_unref(msg);
+	if (!reply) {
+		pam_error(pamh, "D-Bus call failed (%s)", err.message);
+		dbus_error_free(&err);
+		return PAM_AUTH_ERR;
+	}
+
+	// default
     return PAM_AUTH_ERR;
 }
 
--- pam_fdkey.py
+++ pam_fdkey.py
@@ -1,8 +1,33 @@
 import tomllib
 import os
+import sys
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+from gi.repository import GLib
+
+class FDKeyService(dbus.service.Object):
+	def __init__(self, bus_name: str, object_path: str) -> None:
+		super().__init__(bus_name, object_path)
+
+	@dbus.service.method('net.meowcorp.FDKey.Authenticate')
+	def PubKeyAuth(self, uid: int) -> bool:
+		print(f'PubKeyAuth: {uid}')
+		return True
 
 def main() -> None:
-	readConfig()
+	# config: dict = readConfig()
+	# print(config)
+
+	# dbus main loop
+	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+	bus: dbus.bus.BusConnection = dbus.SystemBus()
+	bus_name: dbus.bus.BusName = dbus.service.BusName('net.meowcorp.FDKey', bus=bus)
+	object_path: str = '/net/meowcorp/FDKey/Authentication'
+
+	service = FDKeyService(bus_name, object_path)
+	loop = GLib.MainLoop()
+	loop.run()
 
 def readConfig() -> dict:
 	# check for XDG_CONFIG_HOME, default to ~/.config
@@ -13,22 +38,23 @@ def readConfig() -> dict:
 
 	# make sure user has write access to configDir
 	if not os.access(configDir, os.W_OK):
-		print('Error: no write access to config directory')
+		eprint('Error: no write access to config directory')
 		exit(1)
 
-	# check for pam_fdkey.toml
-	configFile: str = os.path.join(configDir, 'pam_fdkey.toml')
+	# check for pam_fdkey.conf
+	configFile: str = os.path.join(configDir, 'pam_fdkey.conf')
 	if not os.path.isfile(configFile):
-		print('Error: config file not found')
+		eprint('Error: config file not found')
 		exit(1)
 	
 	# read config file
-	with open(configFile, 'r') as f:
+	with open(configFile, 'rb') as f:
 		config: dict = tomllib.load(f)
 
-	print(config)
-
 	return config
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 if __name__ == '__main__':
 	main()
